1ee00611a27a6aa59406a5aafa0f93643dbcf961
Nitro <pablo.benito@rocketmail.com>
2022-02-16 19:47:42 -0300

    Point 3 Resuleto

--- testapi.py
+++ testapi.py
@@ -1,6 +1,10 @@
+import imp
 import json
-from fastapi import FastAPI
 import requests
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from zipfile import ZipFile
+
 
 app = FastAPI()
 
@@ -12,6 +16,8 @@ headers = {}
 response = requests.request("GET", url, headers=headers, data=payload)
 characters = response.json()
 
+file_path = "all_characters.zip"
+
 @app.get("/")
 def index():
     return {"home": "sin datos"}
@@ -21,3 +27,13 @@ def get_characters(species: str, gender: str | None = None):
     if gender:
         return characters
     return characters
+
+@app.get("/download")
+def download_characters():
+    with open('all_characters.json', 'w') as outfile:
+        outfile.write(json.dumps(characters))
+
+    with ZipFile('all_characters.zip', 'w') as download_file:
+        download_file.write('all_characters.json')
+
+    return FileResponse(path=file_path, filename=file_path)
